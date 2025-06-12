5f43a9b6840b8aa4991ab915a58e5943b0f6a992
 <preciada.t@gmail.com>
2025-06-12 22:46:30 +0900

    envファイルに合わせてコード修正、distファイルの内容変更したためコミット

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_BASE_URL_KEY,
   withCredentials: true, // クッキーや認証情報を含める
   headers: {
     'Content-Type': 'application/json',
   },
 })
-
+console.log('API Client created with base URL:', import.meta.env.VITE_BASE_URL_KEY)
 export default apiClient
